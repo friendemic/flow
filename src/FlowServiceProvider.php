3885bd96c936c6a3b9c4aa50f7da520d23bf6ecb
hakula <jsph.niu@gmail.com>
2017-06-09 10:58:08 -0600

    Provider and config

---
+++ src/FlowServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Friendemic\Flow;
+
+use Illuminate\Support\ServiceProvider;
+
+class FlowServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
